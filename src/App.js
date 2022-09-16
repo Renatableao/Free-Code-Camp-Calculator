0742a3896626cfa7f07678edab78bcce560f5076
Renata <renatab_leao@yahoo.com.br>
2022-09-16 11:12:22 -0300

    fixing evaluations bugs/add comment

--- src/App.js
+++ src/App.js
@@ -23,25 +23,29 @@ class App extends React.Component {
 
 
   handleNumbers(event) {
-    if(this.state.output.length > 10 && this.state.output !== "DIGIT LIMIT MET") {
+    //If output is longer then 10 digits
+    if(this.state.output.length > 10 && this.state.output !== "DIGIT LIMIT MET" && this.state.output !== "Misused operator") {
         this.setState({
           prevVal: this.state.output,
           output: 'DIGIT LIMIT MET'
         });
         setTimeout(() => this.setState({ output: this.state.prevVal }), 500);
       }
-      
-    else {
+    //If output is less then 10 digits and is valid number
+    else if(this.state.output !== "Misused operator") {
+      //Initial input or input after result displayed
       if((this.state.output === '0' && (this.state.formula === "" || this.state.formula === "0")) || (/=+/).test(this.state.formula)) {
         this.setState({
           output: event.target.value,
           formula: event.target.value
         })}
-        else if((/[1-9]+/).test(this.state.output)) {
+        //If input only numbers so far, add digit to output
+        else if((/[1-9.]+/).test(this.state.output)) {
           this.setState({
             output:  this.state.output + event.target.value,
             formula: this.state.formula + event.target.value
           })}
+        //If number is pressed after operator
         else if(this.state.output !== "0" && this.state.output !== "DIGIT LIMIT MET") {
           this.setState({
           output:  event.target.value,
@@ -58,6 +62,7 @@ class App extends React.Component {
   }
 
   handleOperation(event) {
+    //Pressing an operator immediately following = starts a new calculation that operates on the result of the previous evaluation
     if((/=+/).test(this.state.formula)) {
       console.log(this.state.result)
       this.setState({
@@ -65,29 +70,34 @@ class App extends React.Component {
         formula: this.state.result + event.target.value
     })
     }
+    //If no numbers are inputed, replace operator with last one pressed
     else if((/[^0-9]+/).test(this.state.output) && (this.state.formula === "" || (/^[-+*/]$/).test(this.state.formula))) {
       this.setState({
         output: event.target.value,
         formula: event.target.value
       })
     }
+    //If operator pressed after number, update display
     else if((/[0-9]+/).test(this.state.output)) {
       this.setState({
         output: event.target.value,
         formula: this.state.formula + event.target.value
       })
     }
+    ////If 2 or more operators are entered consecutively, and last operator entered is negative (-) sign
     else if(event.target.value === "-" && (/[/+*-]/).test(this.state.output) && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
       this.setState({
         output: event.target.value,
         formula: this.state.formula + event.target.value
       })
     }
+    ////If 2 or more operators are entered consecutively, the operation considered is the last operator entered (excluding the negative (-) sign)
     else if(event.target.value !== "-" && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
       this.setState({
         output: event.target.value,
         formula: this.state.formula.replace(/[/*+-]$/, event.target.value)
     })}
+    //If 2 or more operators are entered consecutively, the operation considered is the last operator entered (excluding the negative (-) sign)
     else if(event.target.value !== "-" && (/[0-9]+[+*/][-]$/).test(this.state.formula)) {
       this.setState({
         output: event.target.value,
@@ -104,19 +114,41 @@ class App extends React.Component {
         formula: this.state.formula + event.target.value + "INFINITY"
       })
     }
-    else if((/^[0-9]+[-*+/]$/).test(this.state.formula)) {
+    //If input is only operators or if equation beggins with / or *
+    else if((/^[*/]./).test(this.state.formula) || (/^[-+*/]$/).test(this.state.formula)) {
       this.setState({
-        output: this.state.formula.slice(0,-1),
-        formula: this.state.formula.slice(0,-1) + event.target.value + this.state.formula.slice(0,-1)
+        output: "Misused operator",
+        formula: "Misused operator"
       })
     }
-    else {
-    this.setState({
-      result: toFixed(evaluate(this.state.formula))
-  }, () => {
+    //If equation ends with one operator followed by -, ignore them and proceed with evaluation
+    else if((/.[-*+/][-]$/).test(this.state.formula)) {
       this.setState({
-        output: this.state.result,
-        formula: this.state.formula + event.target.value + this.state.result
+        result: toFixed(evaluate(this.state.formula.slice(0,-2)))
+        }, () => {
+        this.setState({
+          output: this.state.result,
+          formula: this.state.formula.slice(0,-2) + event.target.value + this.state.result
+      })})
+    }
+    //If equation ends with one operator, ignore last one and proceed with evaluation
+    else if((/.[-*+/]$/).test(this.state.formula)) {
+      this.setState({
+        result: toFixed(evaluate(this.state.formula.slice(0,-1)))
+        }, () => {
+        this.setState({
+          output: this.state.result,
+          formula: this.state.formula.slice(0,-1) + event.target.value + this.state.result
+      })})
+    }
+    //If input is valid and there is not already a result displayed
+    else if(this.state.output !== "Misused operator" && (/^[^=]*$/).test(this.state.formula)) {
+      this.setState({
+        result: toFixed(evaluate(this.state.formula))
+        }, () => {
+        this.setState({
+          output: this.state.result,
+          formula: this.state.formula + event.target.value + this.state.result
       })})
     }
   }
@@ -136,7 +168,7 @@ class App extends React.Component {
         formula: this.state.formula + '0' + event.target.value
     })}
     //Do not add decimal point if number has one already
-    else if((/^[^.]*$/).test(this.state.output)) {
+    else if((/^[^.]*$/).test(this.state.output) && this.state.output !== "Misused operator") {
     this.setState({
       output: this.state.output + event.target.value,
       formula: this.state.formula + event.target.value
@@ -154,7 +186,7 @@ class App extends React.Component {
       <main>
         <button id="clear" onClick={this.handleClear} value="">AC</button>
         <button id="divide" onClick={this.handleOperation} value="/">/</button>
-        <button id="multiply" onClick={this.handleOperation} value="*">*</button>
+        <button id="multiply" onClick={this.handleOperation} value="*">x</button>
         <button id="seven" onClick={this.handleNumbers} value="7">7</button>
         <button id="eight" onClick={this.handleNumbers} value="8">8</button>
         <button id="nine" onClick={this.handleNumbers} value="9">9</button>
