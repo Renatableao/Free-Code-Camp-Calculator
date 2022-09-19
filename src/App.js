90429add4b1473f114afa6af6075eb101dbb88a2
Renata <renatab_leao@yahoo.com.br>
2022-09-19 12:54:59 -0300

    add keydown handlers;design

--- src/App.js
+++ src/App.js
@@ -18,11 +18,12 @@ class App extends React.Component {
     this.handleOperation = this.handleOperation.bind(this)
     this.calcResult = this.calcResult.bind(this)
     this.handleDecimal = this.handleDecimal.bind(this)
-    
+    this.handleClick = this.handleClick.bind(this)
+    this.handleKey = this.handleKey.bind(this)    
   }
 
 
-  handleNumbers(event) {
+  handleNumbers(inputValue) {
     //If output is longer then 10 digits
     if(this.state.output.length > 10 && this.state.output !== "DIGIT LIMIT MET" && this.state.output !== "Misused operator") {
         this.setState({
@@ -36,20 +37,20 @@ class App extends React.Component {
       //Initial input or input after result displayed
       if((this.state.output === '0' && (this.state.formula === "" || this.state.formula === "0")) || (/=+/).test(this.state.formula)) {
         this.setState({
-          output: event.target.value,
-          formula: event.target.value
+          output: inputValue,
+          formula: inputValue
         })}
         //If input only numbers so far, add digit to output
         else if((/[1-9.]+/).test(this.state.output)) {
           this.setState({
-            output:  this.state.output + event.target.value,
-            formula: this.state.formula + event.target.value
+            output:  this.state.output + inputValue,
+            formula: this.state.formula + inputValue
           })}
         //If number is pressed after operator
         else if(this.state.output !== "0" && this.state.output !== "DIGIT LIMIT MET") {
           this.setState({
-          output:  event.target.value,
-          formula: this.state.formula + event.target.value
+          output: inputValue,
+          formula: this.state.formula + inputValue
           })}
     }
   }
@@ -61,57 +62,56 @@ class App extends React.Component {
     })
   }
 
-  handleOperation(event) {
+  handleOperation(inputValue) {
     //Pressing an operator immediately following = starts a new calculation that operates on the result of the previous evaluation
     if((/=+/).test(this.state.formula)) {
-      console.log(this.state.result)
       this.setState({
-        output: event.target.value,
-        formula: this.state.result + event.target.value
+        output: inputValue,
+        formula: this.state.result + inputValue
     })
     }
     //If no numbers are inputed, replace operator with last one pressed
     else if((/[^0-9]+/).test(this.state.output) && (this.state.formula === "" || (/^[-+*/]$/).test(this.state.formula))) {
       this.setState({
-        output: event.target.value,
-        formula: event.target.value
+        output: inputValue,
+        formula: inputValue
       })
     }
     //If operator pressed after number, update display
     else if((/[0-9]+/).test(this.state.output)) {
       this.setState({
-        output: event.target.value,
-        formula: this.state.formula + event.target.value
+        output: inputValue,
+        formula: this.state.formula + inputValue
       })
     }
     ////If 2 or more operators are entered consecutively, and last operator entered is negative (-) sign
-    else if(event.target.value === "-" && (/[/+*-]/).test(this.state.output) && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
+    else if(inputValue === "-" && (/[/+*-]/).test(this.state.output) && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
       this.setState({
-        output: event.target.value,
-        formula: this.state.formula + event.target.value
+        output: inputValue,
+        formula: this.state.formula + inputValue
       })
     }
     ////If 2 or more operators are entered consecutively, the operation considered is the last operator entered (excluding the negative (-) sign)
-    else if(event.target.value !== "-" && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
+    else if(inputValue !== "-" && (/[0-9]+[-+*/]$/).test(this.state.formula)) {
       this.setState({
-        output: event.target.value,
-        formula: this.state.formula.replace(/[/*+-]$/, event.target.value)
+        output: inputValue,
+        formula: this.state.formula.replace(/[/*+-]$/, inputValue)
     })}
     //If 2 or more operators are entered consecutively, the operation considered is the last operator entered (excluding the negative (-) sign)
-    else if(event.target.value !== "-" && (/[0-9]+[+*/][-]$/).test(this.state.formula)) {
+    else if(inputValue !== "-" && (/[0-9]+[+*/][-]$/).test(this.state.formula)) {
       this.setState({
-        output: event.target.value,
-        formula: this.state.formula.replace(/[+*/][-]$/, event.target.value)
+        output: inputValue,
+        formula: this.state.formula.replace(/[+*/][-]$/, inputValue)
     })
   }
 }
 
-  calcResult(event) {
+  calcResult(inputValue) {
     //If division by 0
     if((/[0-9]+[/][0]/).test(this.state.formula)) {
       this.setState({
         output: "INFINITY",
-        formula: this.state.formula + event.target.value + "INFINITY"
+        formula: this.state.formula + inputValue + "INFINITY"
       })
     }
     //If input is only operators or if equation beggins with / or *
@@ -128,7 +128,7 @@ class App extends React.Component {
         }, () => {
         this.setState({
           output: this.state.result,
-          formula: this.state.formula.slice(0,-2) + event.target.value + this.state.result
+          formula: this.state.formula.slice(0,-2) + inputValue + this.state.result
       })})
     }
     //If equation ends with one operator, ignore last one and proceed with evaluation
@@ -138,7 +138,7 @@ class App extends React.Component {
         }, () => {
         this.setState({
           output: this.state.result,
-          formula: this.state.formula.slice(0,-1) + event.target.value + this.state.result
+          formula: this.state.formula.slice(0,-1) + inputValue + this.state.result
       })})
     }
     //If input is valid and there is not already a result displayed and is not the first command on calculator
@@ -148,32 +148,109 @@ class App extends React.Component {
         }, () => {
         this.setState({
           output: this.state.result,
-          formula: this.state.formula + event.target.value + this.state.result
+          formula: this.state.formula + inputValue + this.state.result
       })})
     }
   }
   
 
-  handleDecimal(event) {
+  handleDecimal(inputValue) {
     //If there is already a result displayed
     if((/=+/).test(this.state.formula)) {
       this.setState({
-        output: '0' + event.target.value,
-        formula: '0' + event.target.value
+        output: '0' + inputValue,
+        formula: '0' + inputValue
     })}
     //If it is the first input to calculator or if there is only operators, add 0 before .
     else if((this.state.output === "0" && this.state.formula === "") || (/[-+*/]/).test(this.state.output)) {
       this.setState({
-        output: '0' + event.target.value,
-        formula: this.state.formula + '0' + event.target.value
+        output: '0' + inputValue,
+        formula: this.state.formula + '0' + inputValue
     })}
     //Do not add decimal point if number has one already or if output is error
     else if((/^[^.]*$/).test(this.state.output) && this.state.output !== "Misused operator" && this.state.output !== "DIGIT LIMIT MET") {
     this.setState({
-      output: this.state.output + event.target.value,
-      formula: this.state.formula + event.target.value
+      output: this.state.output + inputValue,
+      formula: this.state.formula + inputValue
   })}
   }
+
+handleKey(event) {
+  console.log(event.key)
+  
+  if(event.key === "Enter") {
+    const button = document.getElementsByName(event.key);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    const val = "="
+    this.calcResult(val);
+  }
+  if(event.key === ".") {
+    const button = document.getElementsByName(event.key);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleDecimal(event.key);
+  }
+  if((/[-+*/]/.test(event.key))) {
+    const button = document.getElementsByName(event.key);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleOperation(event.key);
+  }
+  if((/[0-9]/.test(event.key))) {
+    const button = document.getElementsByName(event.key);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleNumbers(event.key);
+  }
+  if(event.key === "Delete") {
+    const button = document.getElementsByName(event.key);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleClear();
+  }
+}
+
+handleClick(event) {
+  if(event.target.value === "=") {
+    const button = document.getElementsByName("Enter");
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.calcResult(event.target.value);
+  }
+  if(event.target.value === ".") {
+    const button = document.getElementsByName(event.target.value);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleDecimal(event.target.value);
+  }
+  if((/[-+*/]/.test(event.target.value))) {
+    const button = document.getElementsByName(event.target.value);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleOperation(event.target.value);
+  }
+  if((/[0-9]/.test(event.target.value))) {
+    const button = document.getElementsByName(event.target.value);
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleNumbers(event.target.value);
+  }
+  if(event.target.value === "") {
+    const button = document.getElementsByName("Delete");
+    button[0].classList.add('active')
+    setTimeout(() => button[0].classList.remove('active'), 150)
+    this.handleClear();
+  }
+}
+
+componentDidMount() {
+  document.addEventListener('keydown', this.handleKey);
+}
+
+componentWillUnmount() {
+  document.removeEventListener('keydown', this.handleKey);
+}
  
 
   render() {
@@ -184,23 +261,23 @@ class App extends React.Component {
         <div id="display">{this.state.output}</div>
       </header>
       <main>
-        <button id="clear" onClick={this.handleClear} value="">AC</button>
-        <button id="divide" onClick={this.handleOperation} value="/">/</button>
-        <button id="multiply" onClick={this.handleOperation} value="*">x</button>
-        <button id="seven" onClick={this.handleNumbers} value="7">7</button>
-        <button id="eight" onClick={this.handleNumbers} value="8">8</button>
-        <button id="nine" onClick={this.handleNumbers} value="9">9</button>
-        <button id="subtract" onClick={this.handleOperation} value="-">-</button>
-        <button id="four" onClick={this.handleNumbers} value="4">4</button>
-        <button id="five" onClick={this.handleNumbers} value="5">5</button>
-        <button id="six" onClick={this.handleNumbers} value="6">6</button>
-        <button id="add" onClick={this.handleOperation} value="+">+</button>
-        <button id="one" onClick={this.handleNumbers} value="1">1</button>
-        <button id="two" onClick={this.handleNumbers} value="2">2</button>
-        <button id="three" onClick={this.handleNumbers} value="3">3</button>
-        <button id="equals" onClick={this.calcResult} value="=">=</button>
-        <button id="zero" onClick={this.handleNumbers} value="0">0</button>
-        <button id="decimal" onClick={this.handleDecimal} value=".">.</button>
+        <button id="clear" onClick={this.handleClick} value="" name="Delete">AC</button>
+        <button id="divide" onClick={this.handleClick} value="/" name="/">/</button>
+        <button id="multiply" onClick={this.handleClick} value="*" name="*">x</button>
+        <button id="seven" onClick={this.handleClick} value="7" name="7">7</button>
+        <button id="eight" onClick={this.handleClick} value="8" name="8">8</button>
+        <button id="nine" onClick={this.handleClick} value="9" name="9">9</button>
+        <button id="subtract" onClick={this.handleClick} value="-" name="-">-</button>
+        <button id="four" onClick={this.handleClick} value="4" name="4">4</button>
+        <button id="five" onClick={this.handleClick} value="5" name="5">5</button>
+        <button id="six" onClick={this.handleClick} value="6" name="6">6</button>
+        <button id="add" onClick={this.handleClick} value="+" name="+">+</button>
+        <button id="one" onClick={this.handleClick} value="1" name="1">1</button>
+        <button id="two" onClick={this.handleClick} value="2" name="2">2</button>
+        <button id="three" onClick={this.handleClick} value="3" name="3">3</button>
+        <button id="equals" onClick={this.handleClick} value="=" name="Enter">=</button>
+        <button id="zero" onClick={this.handleClick} value="0" name="0">0</button>
+        <button id="decimal" onClick={this.handleClick} value="." name=".">.</button>
       </main>
     </div>
   );
